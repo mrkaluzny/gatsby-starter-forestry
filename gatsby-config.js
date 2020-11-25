8e8df50e0a3ed2546541a24ddf365e4a56c29a02
mrkaluzny <wojciech.kaluzny@me.com>
2020-11-25 19:30:50 +0100

    Config updated changes added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,24 @@ module.exports = {
     footer: config.footer,
   },
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // gatsby-remark-relative-images must go before gatsby-remark-images
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          'gatsby-remark-copy-linked-files',
+          {
+            resolve: `gatsby-remark-images`,
+            options: { maxWidth: 800 },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -30,42 +48,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          // gatsby-remark-relative-images must go before gatsby-remark-images
-          {
-            resolve: `gatsby-remark-relative-images`,
-          },
-          'gatsby-remark-copy-linked-files',
-          {
-            resolve: `gatsby-remark-images`,
-            options: { maxWidth: 800 },
-          },
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `Gatsby Starter Blog`,
-    //     short_name: `GatsbyJS`,
-    //     start_url: `/`,
-    //     background_color: `#ffffff`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/assets/gatsby-icon.png`,
-    //   },
-    // },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     'gatsby-transformer-json',
---
+++ src/pages/spoof.md
@@ -0,0 +1,33 @@
+---
+title: Spoof
+template: blocks
+blocks:
+  - template: block__hero
+    component: hero
+    background: transparent
+    title: Forestry's Gatsby Demo
+    subtitle: A Gatsby Starter Built for Forestry
+  - template: block__cta
+    component: cta
+    background: primary
+    title: Create Your Own Gatsby Site
+    subtitle: Use our quick start to import this demo into Forestry
+    button:
+      url: https://app.forestry.io/quick-start?repo=forestryio/gatsby-starter-forestry&branch=master&engine=gatsby
+      text: Get Started
+  - template: block__3col
+    component: 3col
+    title: Henll
+    col1:
+      image: '/images/quickstart.png'
+      title: Henlo friend
+      content: asd asd nasnd kjansdkjnaskjdas
+    col2:
+      image: '/images/quickstart.png'
+      title: ad asd asd asd
+      content: asd asd asd asd asd
+    col3:
+      title: SDsadasdas das da
+      content: 'sdad adasd asd asd '
+      image: '/images/quickstart.png'
+---
